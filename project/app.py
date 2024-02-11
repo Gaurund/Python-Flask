6601635d4a74607287f6e1deee8fa86d0b912bd1
Gaurund <pavel.makushev@gmail.com>
2024-02-11 11:57:12 +0300

    Second task

--- project/app.py
+++ project/app.py
@@ -6,6 +6,14 @@ from flask import Flask
 выводить на экран текст "Hello, World!".
 '''
 
+'''
+Задание №2
+Дорабатываем задачу 1.
+Добавьте две дополнительные страницы в ваше вебприложение:
+○ страницу "about"
+○ страницу "contact".
+'''
+
 app = Flask(__name__)
 
 
@@ -13,6 +21,16 @@ app = Flask(__name__)
 @app.route('/index/')
 def index():
     return 'Hello world'
+
+
+@app.route('/about/')
+def about():
+    return "About me"
+
+
+@app.route('/contacts/')
+def contacts():
+    return "My contacts"
 
 
 if __name__ == '__main__':
