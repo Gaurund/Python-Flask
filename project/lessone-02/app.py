436a15cb2c8dd598d54367902e070f7187f7cbb2
Gaurund <pavel.makushev@gmail.com>
2024-02-14 15:44:30 +0300

    Lesson 2 - task 2

--- project/lessone-02/app.py
+++ project/lessone-02/app.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request
 
 '''
 Задание №1
 Создать страницу, на которой будет кнопка "Нажми меня", при
 нажатии на которую будет переход на другую страницу с
 приветствием пользователя по имени.
+'''
+
+'''
+Задание №2
+Создать страницу, на которой будет изображение и ссылка
+на другую страницу, на которой будет отображаться форма
+для загрузки изображений.
 '''
 
 app = Flask(__name__)
@@ -20,6 +27,16 @@ def index():
 def hello():
     context = {'title': 'Hello!'}
     return render_template('hello.html', **context)
+
+
+@app.route('/load_pic/', methods=['GET', 'POST'])
+def load_pic():
+    if request.method == 'POST':
+        result = request.files.get('load-pic-file')
+        print(result)
+        # return "Файл загружен"
+    context = {'title': 'Загрузка картинки'}
+    return render_template('load_pic.html', **context)
 
 
 app.run()
