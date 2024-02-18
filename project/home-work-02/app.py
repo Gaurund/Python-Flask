7dc01ecd3a8ccb408139f2247a3c4adf20e07921
Gaurund <pavel.makushev@gmail.com>
2024-02-18 16:58:09 +0300

    HW 2 WIP

--- project/home-work-02/app.py
+++ project/home-work-02/app.py
@@ -1,3 +1,5 @@
+from flask import Flask, make_response, render_template
+
 '''
 Задание
 
@@ -12,3 +14,29 @@
  с данными пользователя и произведено перенаправление
  на страницу ввода имени и электронной почты.
 '''
+
+app = Flask(__name__)
+app.secret_key = b'94dc38deee56cf4e4e64530d5c9d09f3e6295e7ee7c3811eb8c2c88f8d76b0db'
+
+
+@app.route('/')
+def index():
+    context = {
+        'title': 'Вход'
+    }
+    return render_template('index.html', **context)
+
+
+@app.route('/hello/', methods=['GET','POST'])
+def hello():
+    context = {
+        'title': 'Приветствие',
+    }
+    response = make_response(render_template('hello.html', **context))
+    response.set_cookie('username', context['name'])
+    response.set_cookie('email', context['email'])
+    return response
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
