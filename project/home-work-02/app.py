0605df57c85298026f3a6afb8cadef2cdbbd35c6
Gaurund <pavel.makushev@gmail.com>
2024-02-18 15:52:37 +0300

    Homework 2 WIP

--- project/home-work-02/app.py
+++ project/home-work-02/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, request, flash, redirect, url_for
 
 '''
 Задание 
@@ -16,6 +16,7 @@ from flask import Flask, render_template, request, flash
 '''
 
 app = Flask(__name__)
+app.secret_key = b'94dc38deee56cf4e4e64530d5c9d09f3e6295e7ee7c3811eb8c2c88f8d76b0db'
 
 
 @app.route('/')
@@ -33,6 +34,14 @@ def submit_post():
 @app.get('/submit')
 def submit_get():
     return render_template('form.html')
+
+
+@app.route('/form/', methods=['GET', 'POST'])
+def form():
+    if request.method == 'POST':
+        flash('Форма успешно отправлена!', 'success')
+        return redirect(url_for('form'))
+    return render_template('flash_form.html')
 
 
 if __name__ == '__main__':
