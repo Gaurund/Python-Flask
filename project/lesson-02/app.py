6e134c4b1f72b7e4c8dd551bf9b7cf7854bade8f
Gaurund <pavel.makushev@gmail.com>
2024-02-18 13:47:15 +0300

    Lesson 2 - After

--- project/lesson-02/app.py
+++ project/lesson-02/app.py
@@ -14,6 +14,16 @@ from flask import Flask, render_template, url_for, request
 для загрузки изображений.
 '''
 
+'''
+Задание №3
+Создать страницу, на которой будет форма для ввода логина
+и пароля
+При нажатии на кнопку "Отправить" будет произведена
+проверка соответствия логина и пароля и переход на
+страницу приветствия пользователя или страницу с
+ошибкой.
+'''
+
 app = Flask(__name__)
 
 
@@ -37,6 +47,11 @@ def load_pic():
         # return "Файл загружен"
     context = {'title': 'Загрузка картинки'}
     return render_template('load_pic.html', **context)
+
+@app.route('/login/')
+def login():
+    context = {'title': 'Страница входа'}
+    return render_template('login.html', **context)
 
 
 app.run()
