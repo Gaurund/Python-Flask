136d287260c4fc8d0c4841f2db6a0a6cdb84c12f
Gaurund <pavel.makushev@gmail.com>
2024-02-20 13:20:03 +0300

    Lecture 3 WIP

--- project/lecture-03/app.py
+++ project/lecture-03/app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from models import db
+from models import db, User, Post, Comment
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'
@@ -9,6 +9,12 @@ db.init_app(app)
 @app.route('/')
 def index():
     return 'Hi!'
+
+
+@app.cli.command('init-db')
+def init_db():
+    db.create_all()
+    print('OK')
 
 
 if __name__ == '__main__':
