e28e80200f1ced2db2c850a8d91a8610694437c2
Gaurund <pavel.makushev@gmail.com>
2024-02-22 21:20:47 +0300

    Homework 4 argparse

--- project/home-work-04/main.py
+++ project/home-work-04/main.py
@@ -12,5 +12,34 @@
   через аргументы командной строки.
 — Программа должна выводить в консоль информацию о времени
   скачивания каждого изображения и общем времени выполнения программы.
-
 '''
+import argparse
+
+
+def download_file(url):
+    pass
+
+
+def download_list_of_files(urls):
+    pass
+
+
+def print_line(messages):
+    for message in messages:
+        print(f'Строка: {message} ')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        prog='FileDownloader',
+        description='The program take in URL strings and request this files.'
+    )
+    # parser.add_argument('-m', '--mode', type=str, nargs=1, help="Выберите режим.")
+    parser.add_argument('line', type=str, nargs='*', help="Введите строку.")
+    args = parser.parse_args()
+    # print(args.mode)
+    # if args.mode[0] == 'a':
+    #     print('Успех')
+    # else:
+    #     print('Nope!')
+    print_line(args.line)
