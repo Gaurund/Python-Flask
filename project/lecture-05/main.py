26062c4fa264a19c73047be3796b2f47089c0150
Gaurund <pavel.makushev@gmail.com>
2024-02-23 15:28:39 +0300

    Lecture 5 12

--- project/lecture-05/main.py
+++ project/lecture-05/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
 
-@app.get("/", response_class=HTMLResponse)
-async def read_root():
-    return "<h1>Hello World</h1>"
+@app.get("/message")
+async def read_message():
+    message = {"message": "Hello World"}
+    return JSONResponse(content=message, status_code=200)
