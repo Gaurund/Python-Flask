3ae8842f45ca391e7f6ee1ff5b4d43509099819b
Gaurund <pavel.makushev@gmail.com>
2024-02-23 15:26:26 +0300

    Lecture 5 11

--- project/lecture-05/main.py
+++ project/lecture-05/main.py
@@ -1,26 +1,9 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/", response_class=HTMLResponse)
 async def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: str = None):
-    if q:
-        return {"item_id": item_id, "q": q}
-    return {"item_id": item_id}
-
-
-@app.get("/items/")
-async def skip_limit(skip: int = 0, limit: int = 10):
-    return {"skip": skip, "limit": limit}
-
-
-@app.get("/users/{user_id}/orders/{order_id}")
-async def read_item(user_id: int, order_id: int):
-    # обработка данных
-    return {"user_id": user_id, "order_id": order_id}
+    return "<h1>Hello World</h1>"
