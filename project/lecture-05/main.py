ce3a3656f270d41b37c41da5b58fcd45dce2e28e
Gaurund <pavel.makushev@gmail.com>
2024-02-23 15:29:44 +0300

    Lecture 5 13

--- project/lecture-05/main.py
+++ project/lecture-05/main.py
@@ -1,10 +1,11 @@
-from fastapi import FastAPI
-from fastapi.responses import JSONResponse
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
 
-@app.get("/message")
-async def read_message():
-    message = {"message": "Hello World"}
-    return JSONResponse(content=message, status_code=200)
+@app.get("/{name}", response_class=HTMLResponse)
+async def read_item(request: Request, name: str):
+    return templates.TemplateResponse("item.html", {"request": request, "name": name})
