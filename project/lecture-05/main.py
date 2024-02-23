51062e944d922915d8a73d4e5d275281dd1b6277
Gaurund <pavel.makushev@gmail.com>
2024-02-23 14:16:29 +0300

    Lecture 5 02

--- project/lecture-05/main.py
+++ project/lecture-05/main.py
@@ -8,3 +8,6 @@ async def root():
     return {'message': 'Hello World123'}
 
 
+@app.get('/items/{item_id}')
+async def read_item(item_id: int, q: str = None):
+    return {'item_id': item_id, 'q': q}
