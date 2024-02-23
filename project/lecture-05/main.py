b5573dc9a866ed785393d17f5e4368dd1ab9b608
Gaurund <pavel.makushev@gmail.com>
2024-02-23 15:40:46 +0300

    Lecture 5 15

--- project/lecture-05/main.py
+++ project/lecture-05/main.py
@@ -3,7 +3,7 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
-templates = Jinja2Templates(directory="templates")
+templates = Jinja2Templates(directory="./project/lecture-05/templates")
 
 
 @app.get("/{name}", response_class=HTMLResponse)
